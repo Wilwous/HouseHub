b74d25393f6768e6b6546e2b5a1a40429d404d95
Wilwous <mick.thomson.tm@gmail.com>
2024-08-28 23:07:46 +0300

    Initial Commit

---
+++ HouseHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HouseHub
+//
+//  Created by Антон Павлов on 28.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
